a90bd9a843bccb8efa836c414c72c569f9e30dbb
banzlovar1 <brettanzlovar@gmail.com>
2022-11-07 19:12:54 -0700

    Updating everything

--- account.py
+++ account.py
@@ -2,11 +2,11 @@ import csv
 
 
 class Account():
-    def __init__(self, name, email, value, total_invested, free_capital, change):
+    def __init__(self, name, email, account_value, total_invested, free_capital, change=0):
         self.name = name
         self.email = email
         self.free_capital = free_capital
-        self.value = value
+        self.account_value = account_value
         self.positions = {}
         self.total_invested = total_invested
         self.change = change
@@ -18,7 +18,7 @@ class Account():
         return self.total_invested
 
     def get_value(self):
-        return self.value
+        return self.account_value
     
     def get_positions(self):
         return self.positions
@@ -28,35 +28,41 @@ class Account():
         self.total_invested += amount
     
     def get_account_summary(self):
-        print(f"name={self.name:<20} email={self.email:<20}         free_capital={str(self.free_capital)}      account_value={str(self.value)}     total_invested={str(self.total_invested)}      account_change={str(self.change)}")
+        print(f"name={self.name:<20} email={self.email:<20}         free_capital={str(self.free_capital)}      account_value={str(self.account_value)}     total_invested={str(self.total_invested)}      account_change={str(self.change)}")
         if self.positions:
-            print("Ticker               Current Price        Shares               Value                %")
+            print("Ticker               Purchase Price       Current Price        Shares               Current Value        %")
             for pos in self.positions:
-                if self.positions[pos]['start_price'] > self.positions[pos]['value']:
-                    change = '-' + str((self.positions[pos]['start_price'] - self.positions[pos]['value']) / self.positions[pos]['start_price'])
+                if self.positions[pos]['start_value'] > self.positions[pos]['cur_value']:
+                    change = '-' + str(round(((self.positions[pos]['start_value'] - self.positions[pos]['cur_value']) / self.positions[pos]['start_value']) * 100, 2))
                 else:
-                    change = str((self.positions[pos]['value'] - self.positions[pos]['start_price']) / self.positions[pos]['start_price'])
-                print(f"{pos:<20} {str(self.positions[pos]['cur_price']):<20} {str(self.positions[pos]['shares']):<20} {str(self.positions[pos]['value']):<20} {change:<20}")
+                    change = str(round(((self.positions[pos]['cur_value'] - self.positions[pos]['start_value']) / self.positions[pos]['start_value']) * 100, 2))
+                print(f"{pos:<20} {str(self.positions[pos]['purchase_price']):<20} {str(self.positions[pos]['cur_price']):<20} {str(self.positions[pos]['shares']):<20} {str(self.positions[pos]['cur_value']):<20} {change:<20}")
 
     def buy_position(self, position):
-        self.free_capital -= position.value
-        if position.ticker in self.positions:
-            self.positions[position.ticker]['cur_price'] = position.cur_price
-            self.positions[position.ticker]['shares'] += position.shares
-            self.positions[position.ticker]['value'] += position.value
-            self.positions[position.ticker]['start_price'] += position.value
+        if position.value > self.free_capital:
+            self.free_capital -= position.value
+            if position.ticker in self.positions:
+                self.positions[position.ticker]['cur_price'] = position.cur_price
+                self.positions[position.ticker]['shares'] += position.shares
+                self.positions[position.ticker]['cur_value'] += position.value
+                self.positions[position.ticker]['start_value'] += position.value
+                # Average out purchase price for repurchase
+                self.positions[position.ticker]['purchase_price'] = (self.positions[position.ticker]['purchase_price'] + position.cur_price) / 2.0
+            else:
+                self.positions[position.ticker] = {}
+                self.positions[position.ticker]['cur_price'] = position.cur_price
+                self.positions[position.ticker]['shares'] = position.shares
+                self.positions[position.ticker]['cur_value'] = position.value
+                self.positions[position.ticker]['start_value'] = position.value
+                self.positions[position.ticker]['purchase_price'] = position.cur_price
         else:
-            self.positions[position.ticker] = {}
-            self.positions[position.ticker]['cur_price'] = position.cur_price
-            self.positions[position.ticker]['shares'] = position.shares
-            self.positions[position.ticker]['value'] = position.value
-            self.positions[position.ticker]['start_price'] = position.value
+            print("Not enough capital to open position")
         
     def sell_position(self, ticker, debug=0):
         if ticker in self.positions:
-            self.free_capital += self.positions[ticker]['value']
+            self.free_capital += self.positions[ticker]['cur_value']
             if debug:
-                print(f"Selling {ticker} giving {self.positions[ticker]['value']} of free cap")
+                print(f"Selling {ticker} giving {self.positions[ticker]['cur_value']} of free cap")
             del self.positions[ticker]
         else:
             print(f"User does not own {ticker}")
@@ -64,38 +70,39 @@ class Account():
     def update_account_value(self):
         stock_value = 0
         for key in self.positions:
-            stock_value += self.positions[key]['value']
-        self.value = stock_value + self.free_capital
+            stock_value += self.positions[key]['cur_value']
+        self.account_value = stock_value + self.free_capital
         
-        if self.total_invested > self.value:
-            self.change = -100 * float((self.total_invested - self.value) / self.total_invested)
+        if self.total_invested > self.account_value:
+            self.change = -100 * float((self.total_invested - self.account_value) / self.total_invested)
         else:
-            self.change = 100 * float((self.value - self.total_invested) / self.total_invested)
+            self.change = 100 * float((self.account_value - self.total_invested) / self.total_invested)
     
     def update_postion(self, ticker, cur_price):
         self.positions[ticker]['cur_price'] = cur_price
-        self.positions[ticker]['value'] = round(self.positions[ticker]['cur_price'] * self.positions[ticker]['shares'], 2)
+        self.positions[ticker]['cur_value'] = round(self.positions[ticker]['cur_price'] * self.positions[ticker]['shares'], 2)
         self.update_account_value()
 
     def save_account(self):
         file_name = self.email.split('@')[0] + '_account_position.csv'
         with open(file_name, 'w', newline='') as file:
             for pos in self.positions:
-                data = [pos ,self.positions[pos]['start_price'],self.positions[pos]['cur_price'],self.positions[pos]['shares'],self.positions[pos]['value']]
+                data = [pos, self.positions[pos]['purchase_price'],self.positions[pos]['cur_price'],self.positions[pos]['start_value'],self.positions[pos]['shares'],self.positions[pos]['cur_value']]
                 writer = csv.writer(file)
                 writer.writerow(data)
             file.close()
         file_name = self.email.split('@')[0] + '_account_summary.csv'
         with open(file_name, 'w', newline='') as file:
-            data = [self.name, self.email, self.value, self.total_invested, self.free_capital, self.change]
+            data = [self.name, self.email, self.account_value, self.total_invested, self.free_capital, self.change]
             writer = csv.writer(file)
             writer.writerow(data)
             file.close()
 
     def load_positions(self, pos):
         self.positions[pos[0]] = {}
-        self.positions[pos[0]]['start_price'] = float(pos[1])
+        self.positions[pos[0]]['purchase_price'] = float(pos[1])
         self.positions[pos[0]]['cur_price'] = float(pos[2])
-        self.positions[pos[0]]['shares'] = float(pos[3])
-        self.positions[pos[0]]['value'] = float(pos[4])
+        self.positions[pos[0]]['start_value'] = float(pos[3])
+        self.positions[pos[0]]['shares'] = float(pos[4])
+        self.positions[pos[0]]['cur_value'] = float(pos[5])
        
--- back_test.py
+++ back_test.py
@@ -82,7 +82,7 @@ def bollinger_bands(data, stockList):
 
     # Sort from low to high RSI as lower RSI means oversold  
     picks.sort(key = lambda x: x[3])
-    return picks
+    return picks[:int(.05*len(picks) if len(picks) < 10 else 10)]
 
 def simple_buy(data, stockList):
     picks = []
@@ -113,14 +113,14 @@ def simple_buy(data, stockList):
 
 def stocks_to_buy(account, picks):
     if account.free_capital > 10: 
-        buying = picks[:int(.05*len(picks))]
+        buying = picks[:int(.05*len(picks) if len(picks) < 10 else 10) ]
         for s in buying:
             new_pos = Position(s[0], s[1], (account.free_capital * .1))
             account.buy_position(new_pos)
 
 def stocks_to_sell(account, stock_data):
+    sell = []
     if account.positions:
-        sell = []
         for pos in account.positions:
             rollingData = stock_data[pos]
             if rollingData.empty:
@@ -142,14 +142,14 @@ def stocks_to_sell(account, stock_data):
             # 1) Value of position has dropped to 5% below start
             # 2) 20MA crosses above 5MA
             # 3) 20MA slope drops to lower then .25 (momentum declining)
-            if account.positions[pos]['value'] < account.positions[pos]['start_price']:
-                if float((account.positions[pos]['start_price'] - account.positions[pos]['value'])/account.positions[pos]['start_price']) > .03:
+            if account.positions[pos]['cur_value'] < account.positions[pos]['start_value']:
+                if float((account.positions[pos]['start_value'] - account.positions[pos]['cur_value'])/account.positions[pos]['start_value']) > .03:
                     sell.append(pos)
             elif lon[-1] > short[-1]:
                 sell.append(pos)
             elif MASlope20 < 0.25:
                 sell.append(pos)
-        return sell
+    return sell
         
         # field_names = ['Ticker', 'Start_Value', 'Sale_Value', 'Account_Value', 'Total_Invested']
         # for ticker in sell:
@@ -161,10 +161,10 @@ def stocks_to_sell(account, stock_data):
         #         f_object.close()
 
 
-def analyze(data, tickers, end):
+def analyze(data, tickers):
     counter = 0
     watchList = []
-    for i in range(0, end):
+    for i in range(0, len(tickers)):
         # Download Ticker info
         trend = data[tickers[i]]
         counter = counter+1
@@ -177,7 +177,7 @@ def analyze(data, tickers, end):
                 watchList.append(tickers[i])
     return bollinger_bands(data, watchList)
 
-def check_postions(stock_data, account):
+def update_postions(account):
     # define selling positions and updating account and positions here
     # conditions
     #   1) Lost 5% on trade
@@ -185,64 +185,99 @@ def check_postions(stock_data, account):
     #   3) Stock goes below 20 MA (crossover specific)
     if account.positions:
         for pos in account.positions:
-            cur_price = stock_data.loc[:,pos].tolist()[-1]
+            cur_price = get_current_price(pos)
             account.update_postion(pos, cur_price)
-        return stocks_to_sell(account, stock_data)
 
-def test_wrapper(stock_data, tickers, account):
-    print(f'Begin Test!!!\n\t{account.name} current value : {account.value}')
-    # Run over 60 data sets for each interval
-    i = 0
-    rows = data.shape[0]
-    while i < rows - 60:
-        picks = []
-        check_postions(data.iloc[i:61+i,:], account)
-        picks = analyze(data.iloc[i:61+i,:], tickers, len(tickers))
-        stocks_to_buy(account, picks)
-        i += 1
-        if i % 10 == 0:
-            account.get_account_summary()
-        if i % 15 == 0:
-            account.add_free_cap(100)
-            account.save_account()
-    account.get_account_summary()
+# def test_wrapper(stock_data, tickers, account):
+#     print(f'Begin Test!!!\n\t{account.name} current value : {account.value}')
+#     # Run over 60 data sets for each interval
+#     i = 0
+#     rows = data.shape[0]
+#     while i < rows - 60:
+#         picks = []
+#         check_postions(data.iloc[i:61+i,:], account)
+#         picks = analyze(data.iloc[i:61+i,:], tickers, len(tickers))
+#         stocks_to_buy(account, picks)
+#         i += 1
+#         if i % 10 == 0:
+#             account.get_account_summary()
+#         if i % 15 == 0:
+#             account.add_free_cap(100)
+#             account.save_account()
+#     account.get_account_summary()
+
+def get_current_price(ticker):
+    ticker_info = yf.Ticker(ticker)
+    return ticker_info.info['currentPrice']
 
 def load_account(name):
-    with open(name+'_account_summary.txt') as file: 
-        line = file.readline()
-    file.close()
-    account_info = line.split(',')
-    user_account = Account(account_info[0],account_info[1],float(account_info[2]),float(account_info[3]),float(account_info[4]),float(account_info[5]))
+    acc = []
+    with open(name+'_account_summary.csv') as file: 
+        reader = csv.reader(file)
+        for row in reader:
+            acc.append(row)
+    user_account = Account(acc[0][0],acc[0][1],float(acc[0][2]),float(acc[0][3]),float(acc[0][4]),float(acc[0][5]))
     pos = []
-    if os.stat(name+'_account_position.csv').st_size != 0:
-        with open(name+'_account_position.csv', 'r') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                pos.append(row)
-        for p in pos:
-            user_account.load_positions(p)
+    if os.path.exists(name+'_account_position.csv'):
+        if os.stat(name+'_account_position.csv').st_size != 0:
+            with open(name+'_account_position.csv', 'r') as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    pos.append(row)
+            for p in pos:
+                user_account.load_positions(p)
     print(f"{name} Account has been successfully loaded\n")
     # user_account.get_account_summary()
     return user_account
 
+def arg_parse(parser_name, args, stock_price_data, tickers):
+    if parser_name != 'create':
+        user = args.user
+        # load account
+        account_user = load_account(user)
+        # update account positions
+        update_postions(account_user)
+        if parser_name == 'analyze':
+            buy = [i[0] for i in analyze(stock_price_data, tickers)]
+            sell = stocks_to_sell(account_user, stock_price_data)
+            print("Stocks to sell")
+            if len(sell):
+                for sk in sell:
+                    if sk not in buy:
+                        print(sk)
+            print("Stocks to buy")
+            b = [i for i in buy if i not in account_user.positions]
+            print(b)
+        if parser_name == 'buy':
+            #buy stock wrapper
+            get_current_price(args.ticker)
+            account_user.buy_position(Position(args.ticker, get_current_price(args.ticker), args.amount))
+            account_user.get_account_summary()
+        elif parser_name == 'sell':
+            account_user.sell_position(args.ticker)
+        elif parser_name == 'summary':
+            account_user.get_account_summary()
+    else:
+        account_user = Account(args.user, args.email, args.capital, args.capital,args.capital)
+    account_user.save_account()
+    
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-# os.chdir(os.path.dirname(os.path.realpath(__file__)))
-
-# df = pd.read_csv('sp500.csv')
-# tickers = df['Symbol'].tolist()
-# fileName = date.today().isoformat() + 'stock_price.csv'
-# volFileName = date.today().isoformat() + 'Volume'+'.csv'
-# # fileName = "2022-10-03stock_price.csv"
-# # volFileName = "2022-10-03Volume.csv"
-# if path.exists(fileName) and path.exists(volFileName):
-#     data = pd.read_csv(fileName)
-#     volume = pd.read_csv(volFileName)
-# else:
-#     data = download(tickers, '3y')
-#     volume = data['Volume']
-#     data = data['Open']
-#     data.to_csv(fileName)
-#     volume.to_csv(volFileName)
+df = pd.read_csv('sp500.csv')
+tickers = df['Symbol'].tolist()
+fileName = date.today().isoformat() + 'stock_price.csv'
+volFileName = date.today().isoformat() + 'Volume'+'.csv'
+# fileName = "2022-10-03stock_price.csv"
+# volFileName = "2022-10-03Volume.csv"
+if path.exists(fileName) and path.exists(volFileName):
+    data = pd.read_csv(fileName)
+    volume = pd.read_csv(volFileName)
+else:
+    data = download(tickers, '3y')
+    volume = data['Volume']
+    data = data['Open']
+    data.to_csv(fileName)
+    volume.to_csv(volFileName)
 
 # df = data.iloc[-60:,:]
 # user = load_account('banzlovar')
@@ -260,18 +295,33 @@ def load_account(name):
 
 parser = argparse.ArgumentParser(description='Stock picker and account manager')
 
-subparser = parser.add_subparsers()
+subparser = parser.add_subparsers(dest='command')
+
+parser_analyze = subparser.add_parser('analyze', help='Upddate and Analyze account holdings')
+parser_analyze.add_argument('-u', '--user', help='Username', required=True)
+
 parser_buy = subparser.add_parser('buy', help="ticker and amount to buy in USD")
 parser_buy.add_argument('-u', '--user', help='Username', required=True)
 parser_buy.add_argument('-t', '--ticker', help='Stock ticker', required=True)
-parser_buy.add_argument('-amt', '--amount', help='amount of stock to buy in USD', required=True)
+parser_buy.add_argument('-a', '--amount', type=float, help='amount of stock to buy in USD', required=True)
 
 parser_sell = subparser.add_parser('sell', help='Stock to sell (full close of position)')
 parser_sell.add_argument('-u', '--user', help='Username', required=True)
 parser_sell.add_argument('-t', '--ticker', help='Stocker ticker to sell', required=True)
 
+parser_summary = subparser.add_parser('summary', help="Account summary")
+parser_summary.add_argument('-u', '--user', help='Username', required=True)
+
+parser_create = subparser.add_parser('create', help='Create account')
+parser_create.add_argument('-u', '--user', help='Username', required=True)
+parser_create.add_argument('-e', '--email', help='Email', required=True)
+parser_create.add_argument('-c', '--capital', help='Capital', required=True)
+
 args = parser.parse_args()
-print(args)
+
+arg_parse(args.command, args, data, tickers)
+# print(args.command)
+
 
 
 

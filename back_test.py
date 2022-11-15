d7a804086f807cea9d4f95f4711c3c1e5fc6c5ed
banzlovar1 <brettanzlovar@gmail.com>
2022-11-14 22:04:06 -0700

    adding test support again

--- back_test.py
+++ back_test.py
@@ -10,6 +10,10 @@ from scipy.stats import linregress
 import pandas_ta as pta
 import csv
 import argparse
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import math
 
 def download(stock, period):
     data = yf.download(stock, period = period)
@@ -62,6 +66,16 @@ def bollinger_bands(data, stockList):
         ds = rollingData.tolist()
         rsi = find_rsi(rollingData).tolist()[-1]
 
+        williams_r_high = max(ds[-14:])
+        williams_r_low = min(ds[-14:])
+        if not math.isnan(williams_r_high) or not math.isnan(williams_r_low):
+            try:
+                williams_r = -100 * ((williams_r_high - ds[-1]) / (williams_r_high - williams_r_low))
+            except ZeroDivisionError:
+                williams_r = 0
+        else:
+            williams_r = 0
+
         dataslope = trenddetector(list(range(0, 3)), ds[-3:])
 
         # Buy signals
@@ -73,6 +87,8 @@ def bollinger_bands(data, stockList):
             picks.append((stockList[i],ds[-1],lower_bollinger[-1], rsi))
         elif short[-1] > lon[-1] and short[-2] < lon[-2]:
             picks.append((stockList[i],ds[-1],lower_bollinger[-1], rsi))
+        # elif williams_r < -70 and dataslope > 0:
+        #     picks.append((stockList[i],ds[-1],lower_bollinger[-1], rsi))
 
     # Sort from low to high RSI
     picks.sort(key = lambda x: x[3])
@@ -134,6 +150,16 @@ def stocks_to_sell(account, stock_data):
             MASlope20 = trenddetector(list(range(0, 3)), lon[-3:])
             price_slope = trenddetector(list(range(0,3)), ds[-3:])
 
+            williams_r_high = max(ds[-14:])
+            williams_r_low = min(ds[-14:])
+            if not math.isnan(williams_r_high) or not math.isnan(williams_r_low):
+                try:
+                    williams_r = -100 * ((williams_r_high - ds[-1]) / (williams_r_high - williams_r_low))
+                except ZeroDivisionError:
+                    williams_r = 0
+            else:
+                williams_r = 0
+
             # Sell triggers
             # 1) Value of position has dropped to 3% below start
             # 2) 20MA crosses above 5MA
@@ -148,6 +174,8 @@ def stocks_to_sell(account, stock_data):
                 sell.append((pos, "20MA sloping down"))
             elif ds[-1] < lower_bollinger[-1]:
                 sell.append((pos, "Price dropped below lower bolinger"))
+            elif williams_r > -40:
+                sell.append((pos, "Williams R Percent indicates over sold"))
     return sell
 
 def analyze(data, tickers):
@@ -171,6 +199,87 @@ def update_postions(account):
         for pos in account.positions:
             cur_price = get_current_price(pos)
             account.update_postion(pos, cur_price)
+
+def email(account, sell, buy):
+    smtp_server = 'smtp.gmail.com'
+    port = 587
+    sender_email = 'programtester456@gmail.com'
+    password = 'wguthxpikodugqzr'
+    reciever_email = account.email
+    message = MIMEMultipart('alternative')
+    message['Subject'] = f"Account Update for {account.name}'s Account"
+    message['From'] = sender_email
+    message['To'] = reciever_email
+    html = f"""\
+    <html>
+        <body>
+            <h2>Account Summary</h2>
+            <h3>Name: {account.name}   Free Capital: {str(account.free_capital)}   Account Value: {str(account.account_value)}   % Change: {str(account.change)}</h3>
+            <table>
+                <tr>
+                    <th>Ticker</th>
+                    <th>Purchase Price</th>
+                    <th>Current Price</th>
+                    <th>Shares</th>
+                    <th>Current Value</th>
+                    <th>% Change</th>
+                </tr>
+    """ 
+    for pos in account.positions:
+        html += f"""
+            <tr>
+                <td>{pos}</td>
+                <td>{account.positions[pos]['purchase_price']}</td>
+                <td>{account.positions[pos]['cur_price']}</td>
+                <td>{account.positions[pos]['shares']}</td>
+                <td>{account.positions[pos]['cur_value']}</td>
+                <td>{str(round(((account.positions[pos]['start_value'] - account.positions[pos]['cur_value']) / account.positions[pos]['start_value']) * -100 if account.positions[pos]['start_value'] > account.positions[pos]['cur_value'] else ((account.positions[pos]['cur_value'] - account.positions[pos]['start_value']) / account.positions[pos]['start_value']) * 100, 2))}</td>
+            </tr>
+        """
+    html += "</table>"
+
+    if len(sell):
+        html += """
+                <h3>Stocks to Sell</h3>
+                <table>
+                    <tr>
+                    <th>Ticker</th>
+                    <th>Current Price</th>
+                    <th>% Change</th>
+                    </tr>
+            """
+        for s in sell:
+            html += f"""
+                    <tr>
+                        <td>{s[0]}</td>
+                        <td>{account.poistions[s[0]]['cur_price']}</td>
+                        <td>{str(round(((account.positions[s[0]]['start_value'] - account.positions[s[0]]['cur_value']) / account.positions[s[0]]['start_value']) * -100 if account.positions[s[0]]['start_value'] > account.positions[s[0]]['cur_value'] else ((account.positions[s[0]]['cur_value'] - account.positions[s[0]]['start_value']) / account.positions[s[0]]['start_value']) * 100, 2))}</td>
+                    </tr>
+            """
+        html += "</table>"
+    if len(buy):
+        html += """
+                <h3>Stocks to Buy</h3>
+                <table>
+                    <tr>
+                    <th>Ticker</th>
+                    <th>Current Price</th>
+                    </tr>
+        """
+        for b in buy:
+            html += f"""
+                <tr>
+                    <td>{b}</td>
+                    <td>{str(get_current_price(b))}</td>
+                </tr>
+            """
+        html += "</table></body></html>"
+    mess = MIMEText(html, 'html')
+    message.attach(mess)
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email,reciever_email,message.as_string())
 
 def get_current_price(ticker):
     ticker_info = yf.Ticker(ticker)
@@ -209,7 +318,7 @@ def arg_parse(parser_name, args, stock_price_data, tickers):
             print("Stocks to sell")
             if len(sell):
                 for sk in sell:
-                    if sk not in buy:
+                    if sk[0] not in buy:
                         print(sk)
             print("Stocks to buy")
             b = [i for i in buy if i not in account_user.positions]
@@ -225,61 +334,76 @@ def arg_parse(parser_name, args, stock_price_data, tickers):
             account_user.get_account_summary()
         elif parser_name == 'summary':
             account_user.get_account_summary()
+        elif parser_name == 'email':
+            # update_postions(account_user)
+            buy = [i[0] for i in analyze(stock_price_data, tickers)]
+            sell = stocks_to_sell(account_user, stock_price_data)
+            act_sell = []
+            if len(sell):
+                for sk in sell:
+                    if sk[0] not in buy:
+                        act_sell.append(sk)
+            email(account_user, act_sell, buy)
     else:
         account_user = Account(args.user, args.email, args.capital, args.capital,args.capital)
     account_user.save_account()
     
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-df = pd.read_csv('sp500.csv')
-tickers = df['Symbol'].tolist()
-fileName = date.today().isoformat() + 'stock_price.csv'
-volFileName = date.today().isoformat() + 'Volume'+'.csv'
-# fileName = "2022-10-03stock_price.csv"
-# volFileName = "2022-10-03Volume.csv"
-if path.exists(fileName) and path.exists(volFileName):
-    data = pd.read_csv(fileName)
-    volume = pd.read_csv(volFileName)
-else:
-    data = download(tickers, '3y')
-    volume = data['Volume']
-    data = data['Open']
-    data.to_csv(fileName)
-    volume.to_csv(volFileName)
+if __name__ == "main":
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+    df = pd.read_csv('sp500.csv')
+    tickers = df['Symbol'].tolist()
+    fileName = date.today().isoformat() + 'stock_price.csv'
+    volFileName = date.today().isoformat() + 'Volume'+'.csv'
+    # fileName = "2022-10-03stock_price.csv"
+    # volFileName = "2022-10-03Volume.csv"
+    if path.exists(fileName) and path.exists(volFileName):
+        data = pd.read_csv(fileName)
+        volume = pd.read_csv(volFileName)
+    else:
+        data = download(tickers, '3y')
+        volume = data['Volume']
+        data = data['Open']
+        data.to_csv(fileName)
+        volume.to_csv(volFileName)
 
 
-parser = argparse.ArgumentParser(description='Stock picker and account manager')
+    parser = argparse.ArgumentParser(description='Stock picker and account manager')
 
-subparser = parser.add_subparsers(dest='command')
+    subparser = parser.add_subparsers(dest='command') 
 
-parser_analyze = subparser.add_parser('analyze', help='Upddate and Analyze account holdings')
-parser_analyze.add_argument('-u', '--user', help='Username', required=True)
+    parser_analyze = subparser.add_parser('analyze', help='Upddate and Analyze account holdings')
+    parser_analyze.add_argument('-u', '--user', help='Username', required=True)
 
-parser_buy = subparser.add_parser('buy', help="ticker and amount to buy in USD")
-parser_buy.add_argument('-u', '--user', help='Username', required=True)
-parser_buy.add_argument('-t', '--ticker', help='Stock ticker', required=True)
-parser_buy.add_argument('-a', '--amount', type=float, help='amount of stock to buy in USD', required=True)
+    parser_buy = subparser.add_parser('buy', help="ticker and amount to buy in USD")
+    parser_buy.add_argument('-u', '--user', help='Username', required=True)
+    parser_buy.add_argument('-t', '--ticker', help='Stock ticker', required=True)
+    parser_buy.add_argument('-a', '--amount', type=float, help='amount of stock to buy in USD', required=True)
 
-parser_sell = subparser.add_parser('sell', help='Stock to sell (full close of position)')
-parser_sell.add_argument('-u', '--user', help='Username', required=True)
-parser_sell.add_argument('-t', '--tickers', nargs='+', help='Stocker ticker to sell', required=True)
+    parser_sell = subparser.add_parser('sell', help='Stock to sell (full close of position)')
+    parser_sell.add_argument('-u', '--user', help='Username', required=True)
+    parser_sell.add_argument('-t', '--tickers', nargs='+', help='Stocker ticker to sell', required=True)
 
-parser_summary = subparser.add_parser('summary', help="Account summary")
-parser_summary.add_argument('-u', '--user', help='Username', required=True)
+    parser_summary = subparser.add_parser('summary', help="Account summary")
+    parser_summary.add_argument('-u', '--user', help='Username', required=True)
 
-parser_create = subparser.add_parser('create', help='Create account')
-parser_create.add_argument('-u', '--user', help='Username', required=True)
-parser_create.add_argument('-e', '--email', help='Email', required=True)
-parser_create.add_argument('-c', '--capital', help='Capital', required=True)
+    parser_create = subparser.add_parser('create', help='Create account')
+    parser_create.add_argument('-u', '--user', help='Username', required=True)
+    parser_create.add_argument('-e', '--email', help='Email', required=True)
+    parser_create.add_argument('-c', '--capital', help='Capital', required=True)
 
-parser_create = subparser.add_parser('add_capital', help='Add free captial to account')
-parser_create.add_argument('-u', '--user', help='Username', required=True)
-parser_create.add_argument('-c', '--capital', help='Capital', required=True)
+    parser_create = subparser.add_parser('add_capital', help='Add free captial to account')
+    parser_create.add_argument('-u', '--user', help='Username', required=True)
+    parser_create.add_argument('-c', '--capital', help='Capital', required=True)
 
-args = parser.parse_args()
+    parser_create = subparser.add_parser('email', help='Email account details and analyzed stocks')
+    parser_create.add_argument('-u', '--user', help='Username', required=True)
 
-arg_parse(args.command, args, data, tickers)
-# print(args.command)
+    args = parser.parse_args()
+
+    arg_parse(args.command, args, data, tickers)
+    # print(args.command)
 
 
 
--- test_wrapper.py
+++ test_wrapper.py
@@ -1,33 +1,93 @@
 # Holds testing code
+from back_test import analyze, stocks_to_buy, stocks_to_sell, load_account, download
+from postion import Position
+from account import Account
+import yfinance as yf
+import pandas as pd
+from os import path
+from datetime import date
+import os
+import numpy as np
+from scipy.stats import linregress
+import pandas_ta as pta
+import csv
+import argparse
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import math
 
-# def test_wrapper(stock_data, tickers, account):
-#     print(f'Begin Test!!!\n\t{account.name} current value : {account.value}')
-#     # Run over 60 data sets for each interval
-#     i = 0
-#     rows = data.shape[0]
-#     while i < rows - 60:
-#         picks = []
-#         check_postions(data.iloc[i:61+i,:], account)
-#         picks = analyze(data.iloc[i:61+i,:], tickers, len(tickers))
-#         stocks_to_buy(account, picks)
-#         i += 1
-#         if i % 10 == 0:
-#             account.get_account_summary()
-#         if i % 15 == 0:
-#             account.add_free_cap(100)
-#             account.save_account()
-#     account.get_account_summary()
+def buy_picks(account, picks, stock_price):
+    if picks:
+        for p in picks:
+            if account.free_capital > 10:
+                pos = Position(p[0], stock_price[p[0]].tolist()[-1], account.free_capital * .1)
+                account.buy_position(pos)
 
-# df = data.iloc[-60:,:]
-# user = load_account('banzlovar')
-# buy = analyze(df,tickers, len(tickers))
-# print('Stocks to buy')
-# for stock in buy:
-#     print(stock[0])
-# sell = check_postions(df, user)
-# user.get_account_summary()
-# print('Stocks to Sell')
-# for s in sell:
-#     if s not in map(lambda x: x[0], buy):
-#         print(s)
-# user.save_account()
+def sell_picks(account, picks, win, trades):
+    if picks:
+        for p in picks:
+            if account.positions[p]['cur_value'] > account.positions[p]['start_value']:
+                win += 1
+            account.sell_position(p)
+            trades += 1
+    return win, trades
+
+
+def update_pos(account, stock_data):
+    if account.positions:
+        for pos in account.positions:
+            cur_price = stock_data[pos].tolist()[-1]
+            account.update_postion(pos, cur_price)
+
+def test_wrapper(stock_data, tickers, account):
+    print(f'Begin Test!!!\n\t{account.name} current value : {account.account_value}')
+    # Run over 60 data sets for each interval
+    i = 0
+    rows = data.shape[0]
+    win = 0
+    trades = 0
+    while i < rows - 60:
+        update_pos(account, stock_data.iloc[i:61+i,:])
+        picks = []
+        sell = stocks_to_sell(account, stock_data.iloc[i:61+i,:])
+        buy = analyze(stock_data.iloc[i:61+i,:], tickers)
+        act_sell = []
+        if len(sell):
+                for sk in sell:
+                    if sk[0] not in buy:
+                        act_sell.append(sk[0])
+        win, trades = sell_picks(account, act_sell, win, trades)
+        buy_picks(account, buy, stock_data.iloc[i:61+i,:])
+        i += 1
+        if i % 10 == 0:
+            account.get_account_summary()
+            print(stock_data.iloc[i:61+i,0].tolist()[-1])
+        if i % 15 == 0:
+            account.add_free_cap(100)
+            account.save_account()
+    account.get_account_summary()
+    win_perc = float((win/trades) * 100)
+    print("Win Percentage: ", win_perc)
+
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+df = pd.read_csv('sp500.csv')
+tickers = df['Symbol'].tolist()
+fileName = date.today().isoformat() + 'stock_price.csv'
+volFileName = date.today().isoformat() + 'Volume'+'.csv'
+# fileName = "2022-10-03stock_price.csv"
+# volFileName = "2022-10-03Volume.csv"
+if path.exists(fileName) and path.exists(volFileName):
+    data = pd.read_csv(fileName)
+    volume = pd.read_csv(volFileName)
+else:
+    data = download(tickers, '5y')
+    volume = data['Volume']
+    data = data['Open']
+    data.to_csv(fileName)
+    volume.to_csv(volFileName)
+
+user = Account('tester', 'tester@gmail.com', 1000, 1000, 1000)
+test_wrapper(data, tickers, user)
